c4f6ae6e3282a13294b0e2e3906d292182707281
retto1337 <kuzminskis2013@gmail.com>
2023-06-03 01:06:20 +0300

    nedodelano rastojanie

--- client/src/pages/informacija.jsx
+++ client/src/pages/informacija.jsx
@@ -5,7 +5,10 @@ import { Container } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import { useState, useEffect } from 'react';
 import '../styles/img.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
+import SendIcon from '@mui/icons-material/Send';
 
 dayjs.extend(isBetweenPlugin);
 
@@ -19,7 +22,31 @@ export default function Dashboard() {
     setInterval(() => setDateState(new Date()), 30000);
   }, []);
   return (
+    
     <> <Container maxWidth="xl" sx={{ display: "flex", height: "150px" }}>
+
+
+<Grid container xs={12} justifyContent="center" mt={10} fontSize="xxx-large">
+
+  <TextField label="Forma" variant="standard" />
+</Grid>
+<Grid item xs={12} md={10} mt={30}>
+<TextField label="Email" />
+<TextField label="Telefons" />
+<TextField
+          id="standard-multiline-static"
+          multiline
+          rows={10}
+          defaultValue="Text"
+          variant="standard"
+        />
+        <Button variant="outlined" >
+             Pasūtīt risinājumu
+        </Button>
+          <Button variant="contained"  endIcon={<SendIcon />}>
+            Vai reģistrēties
+          </Button>
+</Grid>       
       <Grid container xs={12} justifyContent="center" mt={10} fontSize="xxx-large">
         <div>
           {dateState.toLocaleString('en-LV', {
