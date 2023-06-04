ebec9232e29421c80764ae461551f4cb1d29c1b3
retto1337 <kuzminskis2013@gmail.com>
2023-06-04 19:01:59 +0300

    header

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { css, StyleSheet } from 'aphrodite'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import { Link } from "@mui/material";
+
 import '../styles/Social.css';
 import Button from "@mui/material/Button";
 import SendIcon from '@mui/icons-material/Send';
+
 import {
-    faYoutube,
-    faFacebook,
+
+
+ 
    
   
 } from "@fortawesome/free-brands-svg-icons";
@@ -24,17 +26,7 @@ export default function Social() {
 
     return (
         <Container maxWidth="xl" className={css(style.container)}>
-            <Grid container direction="row" justifyContent="flex-end" alignItems="flex-end">
-                        <Grid item xs={12} md={3} mt={30}>
-                    <Button variant="contained" color='primary' fullWidth >
-                    Закажите Решение</Button>
-                    </Grid>
-                    <Grid item xs={12} md={3} mt={30}>
-                    <Button variant="text" endIcon={<SendIcon />}>
-                      Если не решил, посмотри наши работы
-                      </Button>
-                      </Grid>
-            </Grid>
+            
         </Container>
     );
 }
