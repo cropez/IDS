7207bdbec92abeec4f1f75c7f68685e3d6e3b2e9
Dmitrijs Vasilcenko <48312748+cropez@users.noreply.github.com>
2023-06-07 19:23:27 +0300

    bug fixes

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -6,9 +6,8 @@ import Divider from '@mui/material/Divider';
 import Layout from '../components/Layout';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-<<<<<<< HEAD
 
-export default function Bildes() {
+export default function profile() {
   return (
     <Layout>
       <Grid container>
@@ -17,69 +16,12 @@ export default function Bildes() {
             <Typography>
               Profils
             </Typography>
-=======
-export default function bildes() {
-  return (<><Layout>
-
-    <Grid container xs={5} justifyContent="center" mt={10} fontSize="xxx-large">
-      <Typography>
-
-  Profils
-
-      </Typography>
-  
-    </Grid>
-    <Grid container xs={5} justifyContent="center" mt={10} fontSize="xxx-large">
-      <Typography>
-
-  Paroles maiņa
-      </Typography>
-
-    </Grid>
-    <Grid container xs={5} justifyContent="center" mt={10} fontSize="xxx-large">
-      <Typography>
-
-  Dalībnieki
-      </Typography>
-
-    </Grid>
-    <Grid container>
-  
-  <Divider orientation="vertical" flexItem>
-    VERTICAL
-  </Divider>
-
-</Grid>
-
-    <Grid container xs={11} justifyContent="center" mt={-9} fontSize="xxx-large">
-
-      <Typography>
-      Epasts
-      </Typography>
-      
-    </Grid>
-    <Grid container xs={11} justifyContent="center" mt={-10} fontSize="xxx-large">
-
-    
-
-      <Typography>
-      Telefon Nummurs
-           </Typography>
-
-    </Grid>
-    <Grid container xs={11} justifyContent="center" mt={-10} fontSize="xxx-large">
-
-      <Typography>
-        Uzvards
-           </Typography>
->>>>>>> 2fc5946daad9b5f09d15169ad99ad136ad8fc00f
           </Grid>
           <Grid container justifyContent="center" mt={10} fontSize="xxx-large">
             <Typography>
               Paroles maiņa
             </Typography>
           </Grid>
-<<<<<<< HEAD
           <Grid container justifyContent="center" mt={10} fontSize="xxx-large">
             <Typography>
               Dalībnieki
@@ -88,28 +30,27 @@ export default function bildes() {
         </Grid>
         <Divider orientation="vertical" flexItem />
         <Grid item xs={6}>
-          <Grid container justifyContent="center" mt={10}  fontSize="xxx-large">
-            <Typography>
-              Vārds
-            </Typography>
-          </Grid>
-          <Grid container justifyContent="center" mt={5} fontSize="xxx-large">
-            <Typography>
-              Uzvards
-            </Typography>
-          </Grid>
-          <Grid container justifyContent="center" mt={5} fontSize="xxx-large">
+          <Grid container justifyContent="center" mt={30} fontSize="xxx-large">
             <Typography>
               Epasts
             </Typography>
           </Grid>
-          <Grid container justifyContent="center"mt={5}  fontSize="xxx-large">
+          <Grid container justifyContent="center" mt={-10} fontSize="xxx-large">
             <Typography>
               Telefon Nummurs
             </Typography>
           </Grid>
-         
-          <Grid container justifyContent="center" mt={25} fontSize="xxx-large">
+          <Grid container justifyContent="center" mt={-10} fontSize="xxx-large">
+            <Typography>
+              Uzvards
+            </Typography>
+          </Grid>
+          <Grid container justifyContent="center" mt={-10} fontSize="xxx-large">
+            <Typography>
+              Vārds
+            </Typography>
+          </Grid>
+          <Grid container justifyContent="center" mt={45} fontSize="xxx-large">
             <Button xs={12} variant="contained" endIcon={<SendIcon />}>
               Iziet no profila
             </Button>
@@ -117,16 +58,5 @@ export default function bildes() {
         </Grid>
       </Grid>
     </Layout>
-=======
-        </Layout>
-        <Grid container justifyContent="center" mt={-30} fontSize="xxx-large">
-      <Button xs={12} variant="contained" endIcon={<SendIcon />}>
-        Iziet no profila
-      </Button>
-    </Grid>
-
-
-  </>
->>>>>>> 2fc5946daad9b5f09d15169ad99ad136ad8fc00f
   );
 }
